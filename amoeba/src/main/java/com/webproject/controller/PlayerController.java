cfb5f95a3c255bd666227eda250d058a4243fec8
veresaron01 <veresaron01@gmail.com>
2021-09-26 19:51:42 +0200

    oauth2 login works, credentials modified

--- amoeba/src/main/java/com/webproject/controller/PlayerController.java
+++ amoeba/src/main/java/com/webproject/controller/PlayerController.java
@@ -2,6 +2,11 @@ package com.webproject.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +16,24 @@ import java.util.List;
 
 @RestController
 //@RequestMapping("/api/players")
+//@RequestMapping("/players")
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class PlayerController {
 
-    @GetMapping
-    public ResponseEntity<String> getTestHomePage() {
-        return new ResponseEntity("HOME", HttpStatus.ACCEPTED);
-    }
+//    @GetMapping
+//    public ResponseEntity<String> getTestHomePage() {
+//        return new ResponseEntity("HOME", HttpStatus.ACCEPTED);
+//    }
 
+    @GetMapping
+    public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient, @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+
+        return "userName: " + model.getAttribute("userName") + model.getAttribute("clientName") + model.getAttribute("userAttributes")
+                ;
+
+    }
 }
